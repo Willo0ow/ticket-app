17e8a49150191aae66ca37a1a8ee52b321c44dc1
Dorota Hinc <willo0ow@MacBook-Pro-Dorota.local>
2021-09-01 16:09:50 +0200

    basic ticket model, migration, controller

---
+++ database/migrations/2021_09_01_135737_create_tickets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->integer('dept_id');
+            $table->integer('user_id');
+            $table->date('deadline')->nullable();
+            $table->integer('priority');
+            $table->json('assignees')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
