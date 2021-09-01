bd592c33b51b47602034066eb38477866345e7cd
Dorota Hinc <willo0ow@MacBook-Pro-Dorota.local>
2021-09-01 18:56:52 +0200

    add cols to users table, assign dept to a user panel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->nullable();
+            $table->integer('department_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->index(['department_id']);
         });
     }
 
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,8 +1,48 @@
 <template>
-<div>Users</div>
+<div>
+    <div>users</div>
+    <v-btn @click="updateUser">Save</v-btn>
+    <v-select v-model="selectedDept" :items="depts" item-value="id" item-text="name"></v-select>
+    <v-list>
+        <v-list-item v-for="({id, name, department_id}, index ) of users" :key="id">
+            <v-list-item-title>{{index + 1}}. {{name}} - {{department_id}}</v-list-item-title>
+            <v-list-item-action>
+                <v-btn @click="selectUser(id)">Select</v-btn>
+            </v-list-item-action>
+        </v-list-item>
+    </v-list>
+</div>
 </template>
 <script>
 export default {
     name: "Users",
+    data(){
+        return {
+            users : [],
+            depts: [],
+            selectedDept: null,
+            selectedUser: null,
+        }
+    },
+    methods: {
+        async getUsers(){
+            const {data} = await axios.get('/api/users');
+            this.users = data;
+        },
+    async getDepts() {
+      const { data } = await axios.get("/api/departments");
+      this.depts = data;
+    },
+    selectUser(id){
+        this.selectedUser = id;
+    },
+    async updateUser(){
+        await axios.patch(`/api/users/${this.selectedUser}`, {department_id: this.selectedDept})
+    }
+    },
+    async mounted(){
+        await this.getUsers();
+        await this.getDepts();
+    }
 }
 </script>
