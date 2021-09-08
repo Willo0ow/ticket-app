b66560e5868ae18f2e46e0069983c97c98306a13
Willo0ow <dhinc39@gmail.com>
2021-09-08 17:55:17 +0200

    display list of user department tickets

--- resources/js/Pages/DeptTickets.vue
+++ resources/js/Pages/DeptTickets.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>DeptTickets</div>
+    <v-container>
+        <v-card-title>Department Tickets</v-card-title>
+        <v-list v-if="tickets.length">
+            <v-list-item v-for="ticket of tickets" :key="ticket.id">
+                <v-card width="100%" class="my-3">
+                    <v-card-title>{{ticket.title}}</v-card-title>
+                    <v-card-text>{{ticket.content}}</v-card-text>
+                    <v-card-actions>
+                        <v-btn small class="ml-auto">Assign</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-list-item>
+        </v-list>
+    </v-container>
 </template>
 <script>
 import { mapState, mapActions } from "vuex";
